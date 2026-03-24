ea5e5cfd7871b41fe24d0496a612646cbdb87798
Akash Satpute <akash.satpute@comprinno.net>
2026-03-24 14:41:01 +0530

    Fix: only pass session token if it exists

--- comprinno_pr_agent/bedrock/client.py
+++ comprinno_pr_agent/bedrock/client.py
@@ -20,7 +20,7 @@ class BedrockClient:
             region_name=self.region,
             aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
             aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-            aws_session_token=os.getenv('AWS_SESSION_TOKEN')
+            **(({'aws_session_token': os.getenv('AWS_SESSION_TOKEN')} if os.getenv('AWS_SESSION_TOKEN') else {}))
         )
         
         # Context manager will be initialized per PR in analyze_pr
